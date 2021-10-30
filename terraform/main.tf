c92023da4f2226f985dbc4071db104a9e965387e
Arunesh <hello@arunesh.me>
2021-10-30 22:50:09 +0200

    create lighthouse app function

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,20 @@ resource "azurerm_app_service_plan" "main_plan" {
     size = "S1"
   }
 }
+
+resource "azurerm_storage_account" "main_storage" {
+  name                     = "cfperformanceviewer"
+  resource_group_name      = azurerm_resource_group.main_group.name
+  location                 = azurerm_resource_group.main_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_function_app" "lighthouse_app" {
+  name                       = "cf-performance-viewer-lighthouse"
+  resource_group_name        = azurerm_resource_group.main_group.name
+  location                   = azurerm_resource_group.main_group.location
+  app_service_plan_id        = azurerm_app_service_plan.main_plan.id
+  storage_account_name       = azurerm_storage_account.main_storage.name
+  storage_account_access_key = azurerm_storage_account.main_storage.primary_access_key
+}
